880de2dbbf155f7cff58ad3c27f5ec9982690057
erwinnachtegael <erwin@zuidwest.com>
2022-12-17 09:28:14 +0100

    init1

--- YoutubeApp/ContentView.swift
+++ YoutubeApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world1112221!")
+        Text("Hello, world1112353452221!")
             .padding()
     }
 }
